d4f80ee06b4df53f3b41e8617c4979b623c9473c
Hynp <1123598535@qq.com>
2021-01-22 00:35:58 +0800

    setup的执行时机

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,25 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <h3>名字：{{user.name}}</h3>
-  <h3>年龄：{{user.age}}</h3>
-  <h3>性别：{{user.gender}}</h3>
-  <h3>朋友：{{user.friends}}</h3>
-  <button @click="update()">更新数据</button>
+  <h3>{{msg}}</h3>
+  <button @click="msg += '==='">更新</button>
+  <hr>
+  <Child :msg="msg" />
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue';
+import { defineComponent, ref } from 'vue';
+import Child from './components/Child.vue'
 
 // defineComponent函数,目的是定义一个组件，内部可以传入一个配置对象
 export default defineComponent({
   name: 'App',
-  setup(){
-    const obj = {
-      name: '张三',
-      age: 20,
-      friends :{
-        name : '李四',
-        age : '18',
-        friends: ['王五', '赵四'],
-      },
-    }
-
-    //返回的是一个Proxy的代理对象，被代理的目标对象就是obj对象
-    const user = reactive<any>(obj);
-
-    console.log(user);
-
-    const update = () =>{
-      // user.name = '王五',
-      // user.age += 2,
-      // user.friends.friends[0] = '张三'
-      delete user.age
-    }
+  components: {
+    Child
+  },
+  setup() {
+    const msg = ref('Hello vue');
     return {
-      user,update
+      msg
     }
   }
 });
---
+++ src/components/Child.vue
@@ -0,0 +1,26 @@
+<template>
+    <h2>Child子组件</h2>
+    <h3>{{msg}}</h3>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+    name: 'Child',
+    props: ['msg'],
+    //数据初始化的生命周期回调
+    beforeCreate() {
+        console.log('beforeCreate执行了-2');
+        
+    },
+    setup() {
+        //this是undefined
+        console.log('setup执行了-1', this);
+        return{
+
+        }
+    }
+})
+</script>
+<style lang="">
+    
+</style>
