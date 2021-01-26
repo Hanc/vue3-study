5663dd3d8b9a04ced060c6d8b367f58ae315336c
Hynp <1123598535@qq.com>
2021-01-26 23:35:51 +0800

    计算属性和监视

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,59 @@
 <template>
-  <h2>reactive和ref的细节问题</h2>
-  <h1>m1:{{m1}}</h1>
-  <h1>m2:{{m2}}</h1>
-  <h1>m3:{{m3}}</h1>
-  <hr>
-  <button @click="update">更新数据</button>
+  <h2>计算属性和监视</h2>
+  <fieldset>
+    <legend>姓名和操作</legend>
+    姓氏：<input type="text" placeholder="请输入姓氏" v-model="user.firstName"/>
+    <br />
+    名字：<input type="text" placeholder="请输入名字" v-model="user.lastName"/>
+  </fieldset>
+  <fieldset>
+    <legend>计算属性和监视的演示</legend>
+    <legend>姓名和操作</legend>
+    姓氏：<input type="text" placeholder="显示姓名" v-model="fullName1"/>
+    <br />
+    名字：<input type="text" placeholder="显示姓名" v-model="fullName2"/>
+      <br />
+    名字：<input type="text" placeholder="显示姓名" v-model="fullName3"/>
+  </fieldset>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive } from 'vue';
+import { defineComponent, reactive, computed, watch, ref } from 'vue';
 
 // defineComponent函数,目的是定义一个组件，内部可以传入一个配置对象
 export default defineComponent({
   name: 'App',
   setup() {
-    const m1 = ref('abc');
-    const m2 = reactive({
-      name: '路飞',
-      friends :{
-        name: '索隆ls'
+    const user = reactive({
+      firstName : '东方',
+      lastName : '不败'
+    })
+
+    //返回ref类型对象
+    const fullName1 = computed(()=>{
+      return user.firstName + '_' +user.lastName
+    })
+
+    //set和get
+    const fullName2 = computed({
+      get(){
+        return user.firstName + '_' +user.lastName
+      },
+      set(val: string){
+        const names = val.split('_')
+        user.firstName = names[0]
+        user.lastName = names[1]
       }
-    });
-    const m3 = ref({
-      name: '路飞',
-      friends :{
-        name: '索隆ls'
-      }
-    });
-    const update = () =>{
-      m1.value += '==='
-      m2.friends.name = '乔巴'
-      m3.value.name = '娜美'
-    }
+    })
+
+    // 监视指定的数据
+    const fullName3 = ref('')
+    watch(user, ({firstName, lastName}) =>{
+      fullName3.value = firstName+ '_'+ lastName
+    },{immediate:true, deep:true})
+
     return {
-      m1, m2, m3, update
+      user, fullName1, fullName2, fullName3
     }
   }
 });
