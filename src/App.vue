c9c8c4861e4c1bdcdeae1c9957759ce1c7d3f84c
Hynp <1123598535@qq.com>
2021-01-26 23:16:09 +0800

    reactive和ref的细节问题

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <h3>{{msg}}</h3>
-  <button @click="msg += '==='">更新</button>
+  <h2>reactive和ref的细节问题</h2>
+  <h1>m1:{{m1}}</h1>
+  <h1>m2:{{m2}}</h1>
+  <h1>m3:{{m3}}</h1>
   <hr>
-  <Child :msg="msg" msg2= "msg2" @change="change"/>
+  <button @click="update">更新数据</button>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
-import Child from './components/Child.vue'
+import { defineComponent, ref, reactive } from 'vue';
 
 // defineComponent函数,目的是定义一个组件，内部可以传入一个配置对象
 export default defineComponent({
   name: 'App',
-  components: {
-    Child
-  },
   setup() {
-    const msg = ref('Hello vue');
-    function change(txt: string ) {
-      msg.value += txt;
+    const m1 = ref('abc');
+    const m2 = reactive({
+      name: '路飞',
+      friends :{
+        name: '索隆ls'
+      }
+    });
+    const m3 = ref({
+      name: '路飞',
+      friends :{
+        name: '索隆ls'
+      }
+    });
+    const update = () =>{
+      m1.value += '==='
+      m2.friends.name = '乔巴'
+      m3.value.name = '娜美'
     }
     return {
-      msg, change
+      m1, m2, m3, update
     }
   }
 });
